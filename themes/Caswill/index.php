26332aa7c0480c4daf18eeecbbf84211ce475b7d
Jonathan Lister <jonathan@withjandj.com>
2012-03-04 16:50:15 +0000

    not showing date on non-post pages; making titles links and only if on a singular page

--- themes/Caswill/index.php
+++ themes/Caswill/index.php
@@ -6,8 +6,14 @@
 <div id="content" class="grid8col right">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<div class="article">
-		<h2 id="<?php echo $post->post_name; ?>"><?php the_title(); ?></h2>
-		<span class="date"><?php the_time('jS F Y') ?></span>
+		<h2 id="<?php echo $post->post_name; ?>">
+		<?php if(is_singular()) : ?>
+			<?php the_title(); ?>
+		<?php else: ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+		<?php endif; ?>
+		</h2>
+		<?php if(!is_page()) : ?><span class="date"><?php the_time('jS F Y') ?></span><?php endif; ?>
 		<?php // the_category(); ?>
 		<?php // the_tags(); ?>
 		<?php the_content(); ?>
