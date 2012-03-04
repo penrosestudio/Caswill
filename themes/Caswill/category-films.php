d8720ccfb43b116651b2568da0208f384bfda4d1
Joshua <josh.u.war@gmail.com>
2012-03-04 18:28:30 +0000

    made film index title dynamic

--- themes/Caswill/category-films.php
+++ themes/Caswill/category-films.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
 <div id="content" class="grid8col right">
-	<h2>Film Index</h2>
 	<?php global $wp_query;
 	$cat = $wp_query->query_vars['category_name'];
-	$query = query_posts('category_name='.$cat.'&post_type=video&orderby=menu_order&order=ASC');
-	if(have_posts()) : ?>
+	$query = query_posts('category_name='.$cat.'&post_type=video&orderby=menu_order&order=ASC');?>
+	<h2><?php single_cat_title(); ?></h2>
+	<?php if(have_posts()) : ?>
 	<ul id="filmIndex">
 		<?php while (have_posts()) : the_post();
 			$content = get_the_content($post->ID);
