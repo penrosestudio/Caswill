5e2d38d23d786368dba319e83d75169c5acedf26
Jonathan Lister <jonathan@withjandj.com>
2012-03-04 15:16:17 +0000

    adding category page for all films categories; made sure child categories use parent category page

--- themes/Caswill/functions.php
+++ themes/Caswill/functions.php
@@ -8,4 +8,31 @@ if ( function_exists( 'register_nav_menu' ) ) {
 	register_nav_menu( 'films_menu', 'Films Menu' );
 	register_nav_menu( 'information_menu', 'Information Menu' );
 }
+
+
+function load_cat_parent_template()
+{
+    global $wp_query;
+
+    if (!$wp_query->is_category)
+        return true; // saves a bit of nesting
+
+    // get current category object
+    $cat = $wp_query->get_queried_object();
+
+    // trace back the parent hierarchy and locate a template
+    while ($cat && !is_wp_error($cat)) {
+        $template = STYLESHEETPATH . "/category-{$cat->slug}.php";
+
+        if (file_exists($template)) {
+            load_template($template);
+            exit;
+        }
+
+        $cat = $cat->parent ? get_category($cat->parent) : false;
+    }
+}
+add_action('template_redirect', 'load_cat_parent_template');
+
+
 ?>
