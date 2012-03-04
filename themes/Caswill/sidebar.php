61d470f97b6b88b3abe621e1847f35c6138d15cb
Joshua <josh.u.war@gmail.com>
2012-03-04 18:00:49 +0000

    adding apple image

--- themes/Caswill/sidebar.php
+++ themes/Caswill/sidebar.php
@@ -1,6 +1,6 @@
 				<div id="sidebar" class="grid4col left">
 					<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?> <small><?php bloginfo( 'description' ); ?></small></a></h1>
-					
+					<div id="graphic"></div>
 					<?php wp_nav_menu( array(
 						'theme_location' => 'films_menu', 
 						'menu_id' => 'films' 
