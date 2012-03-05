76cc6344a731fb06420f6123e1ea313046b858f3
Joshua <josh.u.war@gmail.com>
2012-03-05 11:15:55 +0000

    adding title and details to top on single vid pages

--- themes/Caswill/single-video.php
+++ themes/Caswill/single-video.php
@@ -3,10 +3,11 @@
 <div id="content" class="grid8col right">
 	<div id="videoPlayer">
 	<?php if(have_posts()) : while (have_posts()) : the_post(); ?>
+	<h2><?php the_title(); ?></h2>
+	<span class="details"><?php echo(types_render_field("video-details")); ?></span>
 	<?php the_content(''); ?>
 	<?php endwhile; endif; ?>
 	</div>	
-	<span class="details"><?php echo(types_render_field("video-details")); ?></span>
 </div>
 
 <?php get_footer(); ?>
