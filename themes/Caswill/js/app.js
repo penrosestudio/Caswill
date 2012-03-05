d88d4a07dbe02c69f6226b3fd722e255a868f814
Jonathan Lister <jonathan@withjandj.com>
2012-03-05 11:45:38 +0000

    keeping videoplayer steady between fades (setting height); made sure videoplayer is hidden when transitioning to a new video image

--- themes/Caswill/js/app.js
+++ themes/Caswill/js/app.js
@@ -1,30 +1,46 @@
-var $ = jQuery;
+var $ = jQuery,
+	initVideoPlayer = function() {
+		$('#videoPlayer').each(function() {
+			$(this).find('.thumbnails ul li').width($(this).width()).show();
+		});
+	
+		$('#playerControl a').click(function(e) { // clicks on the video details
+			e.preventDefault();
+			var $a = $(this),
+				i = $a.index(),
+				$ul = $('#videoPlayer .thumbnails ul'),
+				$playerBox = $('#playerBox'),
+				showThumbnail = function() {
+					$('#videoPlayer .thumbnails ul').each(function() {
+						var $activeLi = $(this).children('li').eq(i);
+							//left = $activeLi.position().left;
+						console.log($activeLi,-i*$activeLi.width());
+						$a.addClass('active').siblings('a').removeClass('active');
+						$(this).fadeOut(function() {	
+							$(this).css('left',-i*$activeLi.width()+'px');
+							$(this).fadeIn();
+						});
+					});
+				};
+			if($a.hasClass('active')) {
+				return false;
+			}
+			$playerBox.fadeOut(showThumbnail);
+				
+		});
+		
+		$('#videoPlayer img').live('click', function() { // clicks on the big thumbnails
+			var t = $(this).prev('pre.hidden').text(),
+				$playerBox = $('#playerBox'),
+				$ul = $('#videoPlayer .thumbnails ul'),
+				showPlayer = function() {
+					$('#playerBox').html(t).fadeIn();
+				};
+			$ul.fadeOut(showPlayer);
+		});
+	};
 
 $(document).ready(function() {
-	$('#playerControl a').click(function(e) {
-		e.preventDefault();
-		var i = $(this).index();
-		
-		$('#videoPlayer .thumbnails ul').each(function() {
-			var $activeLi = $(this).children('li').eq(i),
-				left = $activeLi.position().left;
-			$(this).fadeOut(function() {	
-				$(this).css('left',-left+'px');
-				$(this).fadeIn();
-			});
-		});	
-	});
-	
-	$('#videoPlayer').each(function() {
-		$(this).find('.thumbnails ul li').width($(this).width()).show();
-	});
-	
-	$('#videoPlayer img').live('click', function() {
-		var t = $(this).prev('pre.hidden').text();
-		$('#videoPlayer .thumbnails ul').fadeOut(function() {
-			$('#playerBox').html(t);	
-		});
-	});
-
+	initVideoPlayer();
 });
 
