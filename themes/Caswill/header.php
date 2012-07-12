f7c641d753c5fcd5a6df515cefb9279e7aa0ab60
Joshua <josh.u.war@gmail.com>
2012-07-12 17:45:40 +0100

    undoing that last thing

--- themes/Caswill/header.php
+++ themes/Caswill/header.php
@@ -9,17 +9,6 @@
 			document.documentElement.className = "js";
 		</script>
 		<?php wp_head(); ?>
-		<script type="text/javascript">		
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-33347231-1']);
-		  _gaq.push(['_trackPageview']);
-		
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-		</script>
 	</head>
 	<body>
 		<div id="wrapper">
