1279d2aabeddc04d5320c5e5f5d507a5d8eda483
Joshua <josh.u.war@gmail.com>
2013-01-18 12:46:06 +0000

    applying redraw styles

--- themes/Caswill/footer.php
+++ themes/Caswill/footer.php
@@ -1,5 +1,7 @@
 			</div>
+			<div class="push"></div>
         </div>
+        <div class="footer"></div>
         <script type="text/javascript" src="http://use.typekit.com/weo5bjr.js"></script>
 		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 		<?php wp_footer(); ?>
--- themes/Caswill/home.php
+++ themes/Caswill/home.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+<div id="tree"></div>
 <?php get_sidebar(); ?>
 <div id="content" class="grid8col right">
 
@@ -33,8 +34,9 @@
 	<div id="playerControl">
 		<?php $i=0;
 		foreach($videos as $post) : ?>
-		<?php if($i==0) : $aClass="active"; else : $aClass=""; endif; ?>
-		<a href="<?php the_permalink(); ?>" class="<?php echo $aClass; ?>"><?php the_title(); ?>
+		<?php $numberClass = $i; ?>
+		<?php if($i==0) : $aClass="active "; else : $aClass=""; endif; ?>
+		<a href="<?php the_permalink(); ?>" class="<?php echo $aClass; echo "number"; echo $i; ?>"><?php the_title(); ?>
 			<!-- <span class="details"><?php echo(types_render_field("video-details")); ?></span> -->
 			<span class="details"><?php 
 			$parentcat = get_category_id('Films');
